b8e33a3600c60553e508290db375c89004c16656
Sylvia <sylvia@donchev.is>
2022-08-23 18:46:33 +0200

    finish part1

--- src/EventList.js
+++ src/EventList.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import Event from "./Event";
 
 class EventList extends Component {
   render() {
-    return <ul className="EventList"></ul>;
+    const { events } = this.props;
+    return (
+      <ul className="EventList">
+        {events.map((event) => (
+          <li key={event.id}>
+            <Event event={event} />
+          </li>
+        ))}
+      </ul>
+    );
   }
 }
 
