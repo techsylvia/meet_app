2c7f5e273fa41cda8119be9627899095e199120a
Sylvia <sylvia@donchev.is>
2022-08-25 18:38:31 +0200

    update events added

--- src/App.js
+++ src/App.js
@@ -30,4 +30,15 @@ class App extends Component {
   }
 }
 
+updateEvents = (location) => {
+  getEvents().then((events) => {
+    const locationEvents = events.filter(
+      (event) => event.location === location
+    );
+    this.setState({
+      events: locationEvents,
+    });
+  });
+};
+
 export default App;
