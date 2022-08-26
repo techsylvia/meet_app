e5eead10bcc1ea90750e2c508ab230679b69e064
Sylvia <sylvia@donchev.is>
2022-08-26 22:10:00 +0200

    axios get url

--- src/api.js
+++ src/api.js
@@ -26,7 +26,9 @@ export const getAccessToken = async () => {
     const searchParams = new URLSearchParams(window.location.search);
     const code = await searchParams.get("code");
     if (!code) {
-      const results = await axios.get("YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT");
+      const results = await axios.get(
+        "https://qlsc0n8dqd.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
       const { authUrl } = results.data;
       return (window.location.href = authUrl);
     }
