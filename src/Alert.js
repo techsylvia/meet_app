15f6f91b6e9316a2ef4cbbc337a3ad8a42cc2d73
Sylvia <sylvia@donchev.is>
2022-08-31 19:28:37 +0200

    Child Alert components created

--- src/Alert.js
+++ src/Alert.js
@@ -5,6 +5,47 @@ class Alert extends Component {
     super(props);
     this.color = null;
   }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+    };
+  };
+
+  render() {
+    return (
+      <div className="Alert">
+        <p style={this.getStyle()}>{this.props.text}</p>
+      </div>
+    );
+  }
+}
+class InfoAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = "blue";
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+    };
+  };
+  render() {
+    return (
+      <div className="Alert">
+        <p style={this.getStyle()}>{this.props.text}</p>
+      </div>
+    );
+  }
+}
+
+class WarningAlert extends Component {
+  constructor(props) {
+    super(props);
+    this.color = "orange";
+  }
+
   getStyle = () => {
     return {
       color: this.color,
@@ -20,40 +61,25 @@ class Alert extends Component {
   }
 }
 
-class InfoAlert extends Alert {
+class ErrorAlert extends Component {
   constructor(props) {
     super(props);
-    this.color = "blue";
+    this.color = "red";
   }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+    };
+  };
 
   render() {
     return (
-      <div className="CitySearch">
-        <InfoAlert text={this.state.infoText} />
-        ...
+      <div className="Alert">
+        <p style={this.getStyle()}>{this.props.text}</p>
       </div>
     );
   }
-  handleInputChanged = (event) => {
-    const value = event.target.value;
-    this.setState({ showSuggestions: true });
-    const suggestions = this.props.locations.filter((location) => {
-      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-    });
-    if (suggestions.length === 0) {
-      this.setState({
-        query: value,
-        infoText:
-          "We can not find the city you are looking for. Please try another city",
-      });
-    } else {
-      return this.setState({
-        query: value,
-        suggestions,
-        infoText: "",
-      });
-    }
-  };
 }
 
-export default InfoAlert;
+export { InfoAlert };
