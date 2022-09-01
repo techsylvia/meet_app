372b0f0738ed10ca4527eb0af05bced4fd2be0de
Sylvia <sylvia@donchev.is>
2022-09-01 16:07:02 +0200

    styling in alert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import { ErrorAlert } from "./Alert";
+import "./App.css";
 
 export class NumberOfEvents extends Component {
+  state = {
+    numberOfEvents: 32,
+    infoText: "",
+  };
+
   handleInputChanged = (event) => {
     const value = event.target.value;
-    this.setState({ numOfEvents: value });
+    if (value >= 33 || value <= 0) {
+      this.setState({
+        numOfEvents: value,
+        infoText: "Please choose a number between 1 and 32",
+      });
+    } else {
+      this.setState({
+        numberOfEvents: event.target.value,
+        infoText: " ",
+      });
+    }
+    this.props.updateEvents(undefined, value);
   };
-  state = { numOfEvents: 32 };
+
   render() {
     return (
       <Form>
@@ -19,10 +36,10 @@ export class NumberOfEvents extends Component {
             value={this.state.numOfEvents}
             onChange={this.handleInputChanged}
           />
+          <div>
+            <ErrorAlert text={this.state.infoText} />
+          </div>
         </Form.Group>
-        <div>
-          <ErrorAlert text={this.state.infoText} />
-        </div>
       </Form>
     );
   }
